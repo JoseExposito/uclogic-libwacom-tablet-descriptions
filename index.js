JoseExposito/uclogic-libwacom-tablet-descriptions

commit ba6c5b164e4cb8ed41b6158c147472becab5cb77

Add more product IDs
HUION added a couple of product IDs to their collection.
Match against them and add a comment indicating that the DeviceMatch
line needs to be manually fixed.

index.js
@@ -92,7 +92,8 @@ const generateTabletDescriptionFile = (firmware, values) => {
 Name=${name}
 ModelName=${model_name}
 Class=${tabletClass}
-DeviceMatch=usb|256c|006e||${firmware};usb|256c|006d||${firmware};
+# Product ID is unknown, please fix
+DeviceMatch=usb|256c|006e||${firmware};usb|256c|006d||${firmware};usb|256c|006f||${firmware};usb|256c|0064||${firmware};
 Width=9  # autogenerated value, please fix
 Height=6  # autogenerated value, please fix
 Layout=

commit f3fccce9aec75d10b35e9650afa221ecb4e1f24b

Move DeviceMatch generation to its own function
Fixes: ba6c5b164e4c ("Add more product IDs")

index.js
@@ -47,6 +47,10 @@ const extractDriver = async (driverPath) => {
     await exec(`tar -xf ${path.resolve(TMP_PATH, 'data.tar.xz')} -C ${TMP_PATH}`);
 }
 
+const getDeviceMatch = (firmware) => (
+    `usb|256c|006e||${firmware};usb|256c|006d||${firmware};usb|256c|006f||${firmware};usb|256c|0064||${firmware};`
+);
+
 const generateTabletDescriptionFile = (firmware, values) => {
     const buttonChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'];
     const evdevCodes = ['BTN_0', 'BTN_1', 'BTN_2', 'BTN_3', 'BTN_4', 'BTN_5', 'BTN_6', 'BTN_7', 'BTN_8', 'BTN_9', 'BTN_SOUTH', 'BTN_EAST', 'BTN_C', 'BTN_NORTH', 'BTN_WEST', 'BTN_Z', 'BTN_TL', 'BTN_TR', 'BTN_TL2', 'BTN_TR2'];
@@ -93,7 +97,7 @@ Name=${name}
 ModelName=${model_name}
 Class=${tabletClass}
 # Product ID is unknown, please fix
-DeviceMatch=usb|256c|006e||${firmware};usb|256c|006d||${firmware};usb|256c|006f||${firmware};usb|256c|0064||${firmware};
+DeviceMatch=${getDeviceMatch(firmware)}
 Width=9  # autogenerated value, please fix
 Height=6  # autogenerated value, please fix
 Layout=
@@ -140,7 +144,7 @@ const saveTabletDescriptionFile = (resultsPath, firmware, {tabletName, tabletDes
         const contents = fs.readFileSync(filePath, 'utf-8');
         contents.split('\n').forEach((l) => {
             if (l.startsWith('DeviceMatch=')) {
-                newDescription += `${l}usb|256c|006e||${firmware};usb|256c|006d||${firmware};\n`;
+                newDescription += `${l}${getDeviceMatch(firmware)}\n`;
             } else {
                 newDescription += `${l}\n`;
             }